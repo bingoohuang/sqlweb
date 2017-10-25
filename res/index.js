192f2d48db1662e79cd467ab4797b4656caf75a2
bingoo <bingoo.huang@gmail.com>
2017-10-25 11:18:16 +0800

    paste as plain text in contentEditable

--- res/index.js
+++ res/index.js
@@ -769,5 +769,18 @@
         $(this).select()
     })
 
-})
-()
+    $(document).on('paste','[contenteditable]', function(e) {
+        e.preventDefault();
+        var text = '';
+        if (e.clipboardData || e.originalEvent.clipboardData) {
+            text = (e.originalEvent || e).clipboardData.getData('text/plain');
+        } else if (window.clipboardData) {
+            text = window.clipboardData.getData('Text');
+        }
+        if (document.queryCommandSupported('insertText')) {
+            document.execCommand('insertText', false, text);
+        } else {
+            document.execCommand('paste', false, text);
+        }
+    });
+})()
