dd6b5cf3f48d9c3335bbf874b0e1d007e5438b9e
bingoohuang <bingoo.huang@gmail.com>
2017-12-19 22:35:01 +0800

    remove unnecessary commas

--- res/index.js
+++ res/index.js
@@ -779,17 +779,17 @@
     })
 
     $(document).on('paste','[contenteditable]', function(e) {
-        e.preventDefault();
-        var text = '';
+        e.preventDefault()
+        var text = ''
         if (e.clipboardData || e.originalEvent.clipboardData) {
-            text = (e.originalEvent || e).clipboardData.getData('text/plain');
+            text = (e.originalEvent || e).clipboardData.getData('text/plain')
         } else if (window.clipboardData) {
-            text = window.clipboardData.getData('Text');
+            text = window.clipboardData.getData('Text')
         }
         if (document.queryCommandSupported('insertText')) {
-            document.execCommand('insertText', false, text);
+            document.execCommand('insertText', false, text)
         } else {
-            document.execCommand('paste', false, text);
+            document.execCommand('paste', false, text)
         }
-    });
+    })
 })()
