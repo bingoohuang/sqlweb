9fe3f8307678ea55b44e59a3938127be66e52b8d
bingoo <bingoo.huang@gmail.com>
2017-12-29 10:20:20 +0800

    refactor: seperate checkboxEditable.js

---
+++ res/checkboxEditable.js
@@ -0,0 +1,56 @@
+(function () {
+    $.toggleRowEditable = function () {
+        var rowChecked = $(this).prop('checked')
+        var dataCells = $(this).parents('tr').find('td.dataCell')
+        if (!rowChecked) {
+            dataCells.attr('contenteditable', false)
+                .unbind('dblclick').unbind('blur')
+            return
+        }
+
+        dataCells.dblclick(function () {
+            var $this = $(this)
+            if (!$this.attr('old')) {
+                $this.attr('old', $this.text())
+            }
+            $this.attr('contenteditable', true)
+                .focus()
+                .keydown(function (event) {
+                    var keyCode = event.keyCode || event.which
+                    if (keyCode == 13 && event.ctrlKey) {
+                        $this.blur()
+                    }
+                })
+        }).blur(function () {
+            var $this = $(this)
+            $this.attr('contenteditable', false)
+            if ($this.attr('old') == $this.text()) {
+                $this.removeAttr('old').removeClass('changedCell')
+            } else {
+                $this.addClass('changedCell')
+            }
+
+            $this.toggleClass('nullCell', '(null)' == $this.text())
+        })
+    }
+
+    function checkboxEditableChange(checkboxEditable) {
+        var edittable = checkboxEditable.prop('checked')
+        checkboxEditable.parent().find('span.editButtons').toggle(edittable)
+        var dataTable = checkboxEditable.parents('div.divResult').find('table.queryResult')
+        dataTable.find('.chk').toggle(edittable)
+        var rowCheckboxes = dataTable.find('.dataRow').find('input[type=checkbox]')
+        rowCheckboxes.unbind('click')
+        if (edittable) {
+            rowCheckboxes.click($.toggleRowEditable)
+        }
+    }
+
+    $.attachEditableEvent = function (queryResultId) {
+        var checkboxEditable = $('#checkboxEditable' + queryResultId)
+        checkboxEditableChange(checkboxEditable)
+        checkboxEditable.click(function () {
+            checkboxEditableChange(checkboxEditable)
+        })
+    }
+})()
--- res/index.js
+++ res/index.js
@@ -1,6 +1,4 @@
 (function () {
-
-
     function attachSaveUpdatesEvent(result) {
         var thisQueryResult = queryResultId
         $('#saveUpdates' + thisQueryResult).click(function () {
@@ -44,68 +42,13 @@
         })
     }
 
-    function toggleRowEditable() {
-        var rowChecked = $(this).prop('checked')
-        var dataCells = $(this).parents('tr').find('td.dataCell')
-        if (!rowChecked) {
-            dataCells.attr('contenteditable', false)
-                .unbind('dblclick').unbind('blur')
-            return
-        }
-
-        dataCells.dblclick(function () {
-            var $this = $(this)
-            if (!$this.attr('old')) {
-                $this.attr('old', $this.text())
-            }
-            $this.attr('contenteditable', true)
-                .focus()
-                .keydown(function (event) {
-                    var keyCode = event.keyCode || event.which
-                    if (keyCode == 13 && event.ctrlKey) {
-                        $this.blur()
-                    }
-                })
-        }).blur(function (event) {
-            var $this = $(this)
-            $this.attr('contenteditable', false)
-            if ($this.attr('old') == $this.text()) {
-                $this.removeAttr('old').removeClass('changedCell')
-            } else {
-                $this.addClass('changedCell')
-            }
-
-            $this.toggleClass('nullCell', '(null)' == $this.text())
-        })
-    }
-
-    function checkboxEditableChange(checkboxEditable) {
-        var edittable = checkboxEditable.prop('checked')
-        checkboxEditable.parent().find('span.editButtons').toggle(edittable)
-        var dataTable = checkboxEditable.parents('div.divResult').find('table.queryResult')
-        dataTable.find('.chk').toggle(edittable)
-        var rowCheckboxes = dataTable.find('.dataRow').find('input[type=checkbox]')
-        rowCheckboxes.unbind('click')
-        if (edittable) {
-            rowCheckboxes.click(toggleRowEditable)
-        }
-    }
-
-    function attachEditableEvent() {
-        var checkboxEditable = $('#checkboxEditable' + queryResultId)
-        checkboxEditableChange(checkboxEditable)
-        checkboxEditable.click(function () {
-            checkboxEditableChange(checkboxEditable)
-        })
-    }
-
     function copyRows($checkboxes) {
         $checkboxes.each(function (index, checkbox) {
             var $tr = $(checkbox).parents('tr')
             $tr.find(':checked').prop("checked", false)
             var $clone = $tr.clone().addClass('clonedRow')
             $clone.insertAfter($tr)
-            $clone.find('input[type=checkbox]').click(toggleRowEditable).click()
+            $clone.find('input[type=checkbox]').click($.toggleRowEditable).click()
         })
     }
 
@@ -230,7 +173,7 @@
         }).toggle($(collapseDiv).height() >= 300)
     }
 
-    $.tableCreate = function(result, sql, resultId) {
+    $.tableCreate = function (result, sql, resultId) {
         var rowUpdateReady = result.TableName && result.TableName != ""
 
         ++queryResultId
@@ -247,7 +190,7 @@
         attachOpsResultDivEvent()
 
         if (rowUpdateReady) {
-            attachEditableEvent()
+            $.attachEditableEvent(queryResultId)
             attachCopyRowsEvent()
             attachDeleteRowsEvent()
             $.attachRowTransposesEvent(queryResultId)
