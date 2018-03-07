ca0daee7c36fe77c685956eac725eacb3e869f36
bingoo <bingoo.huang@gmail.com>
2018-03-07 17:50:36 +0800

    sql can use last result as in clause in multiple sql runs

--- res/sqlAjax.js
+++ res/sqlAjax.js
@@ -1,19 +1,109 @@
 (function () {
+
     $.executeMultiSqlsAjax = function (sql, resultId) {
         var sqls = $.splitSqls(sql, ';')
-
-        $.executeQueryAjax(sqls[0], null, sqls, 0)
+        var executeResultContext = []
+        $.executeQueryAjax(sqls[0], null, sqls, 0, executeResultContext)
     }
 
-    $.executeQueryAjax = function (sql, resultId, sqls, nextIndex) {
+    /*
+    {
+        "Headers": [ "USER_ID", "OPEN_ID", "MOBILE", "TYPE", "INVITER_ID" ],
+        "Rows": [
+        [ "1", "243894407111118848", "oWKxwxCSZouXYylrr2-SRj16Ibyg", "15168593302", "0", "(null)" ]
+        ],
+        "Error": "",
+        "ExecutionTime": "2018-03-07 16:53:45.211",
+        "CostTime": "8.141423ms",
+        "DatabaseName": "dwtdcszfa2bfj1pkt",
+        "TableName": "tt_f_user",
+        "PrimaryKeysIndex": [ 0 ],
+        "Msg": ""
+    }
+     */
+
+    function parseFieldName(sql, resultLastPos, keyLength, leftHolder) {
+        var rightPart = $.trim(sql.substring(resultLastPos + keyLength))
+        var len = rightPart.length
+        for (var i = 0; i < len; ++i) {
+            var ch = rightPart[i]
+            if (ch === ' ' || ch === '\t' || ch === '\n') {
+                leftHolder.left = rightPart.substring(i)
+                return rightPart.substring(0, i)
+            }
+        }
+        leftHolder.left = ''
+        return rightPart
+    }
+
+    function findFieldName(resultLast, fieldName) {
+        for (var i = 0; i < resultLast.Headers.length; ++i) {
+            if (resultLast.Headers[i].toUpperCase() === fieldName) {
+                return i
+            }
+        }
+        return -1;
+    }
+
+    function createValues(fieldNameIndex, resultLast) {
+        var values = []
+        if (fieldNameIndex >= 0 && resultLast.Rows && resultLast.Rows.length) {
+            for (var i = 0; i < resultLast.Rows.length; ++i) {
+                values.push(resultLast.Rows[i][fieldNameIndex + 1])
+            }
+        }
+        return values.length > 0 ? "('" + values.join("','") + "')" : "('')"
+    }
+
+    function translateOne(result, sql, executeResultContext, lastIndex, resultPos, keyLength) {
+        var leftHolder = {}
+        var fieldName = parseFieldName(sql, resultPos, keyLength, leftHolder).toUpperCase()
+        var fieldNameIndex = findFieldName(result, fieldName)
+        var joinedValues = createValues(fieldNameIndex, result)
+
+        return sql.substring(0, resultPos) + joinedValues
+            + translateSqlWithLastResults(leftHolder.left, executeResultContext, lastIndex)
+    }
+
+    function translateSqlWithLastResults(sql, executeResultContext, lastIndex) {
+        var resultPos = sql.indexOf('_result_')
+        if (resultPos > 0) {
+            if (sql.indexOf('_result_last.') === resultPos) {
+                var result = executeResultContext['_result_last']
+                return translateOne(result, sql, executeResultContext, lastIndex, resultPos, '_result_last.'.length)
+            } else {
+                for (var i = lastIndex - 1; i >= 0; --i) {
+                    var key = '_result_' + i + '.';
+                    if (sql.indexOf(key) === resultPos) {
+                        var result = executeResultContext['_result_' + i]
+                        return translateOne(result, sql, executeResultContext, lastIndex, resultPos, key.length)
+                    }
+                }
+            }
+        }
+
+        return sql;
+    }
+
+    $.executeQueryAjax = function (sql, resultId, sqls, nextIndex, executeResultContext) {
+        if (sqls && nextIndex > 0) {
+            sql = translateSqlWithLastResults(sql, executeResultContext, nextIndex)
+        }
+
         $.ajax({
             type: 'POST',
             url: pathname + "/query",
             data: {tid: activeMerchantId, sql: sql},
             success: function (content, textStatus, request) {
                 $.tableCreate(content, sql, resultId)
+
+                if (sqls) {
+                    executeResultContext['_result_' + nextIndex] = content
+                    executeResultContext['_result_last'] = content
+                }
+
                 if (sqls && (nextIndex + 1) < sqls.length) {
-                    $.executeQueryAjax(sqls[nextIndex + 1], resultId, sqls, nextIndex + 1)
+                    $.executeQueryAjax(sqls[nextIndex + 1], resultId, sqls, nextIndex + 1, executeResultContext)
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
