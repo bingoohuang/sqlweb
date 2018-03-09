1ddc76b6370e11b1da757e8ae57cfc9a64c027f7
bingoo <bingoo.huang@gmail.com>
2018-03-09 19:55:37 +0800

    appending sql to editor instead of replacing

--- res/showCreateTable.js
+++ res/showCreateTable.js
@@ -1,13 +1,28 @@
-(function(){
+(function () {
+    $.appendSqlToSqlEditor = function (sql) {
+        var formattedSql = sqlFormatter.format(sql, {language: 'sql'})
+        var codeMirror = $.sqlCodeMirror
+        var value = $.trim(codeMirror.getValue())
+        var newValue = ''
+        if (value !== '') {
+            if (value.substring(value.length - 1) === ';') {
+                newValue = value + '\n\n'
+            } else {
+                newValue = value + ';\n\n'
+            }
+
+        }
+        codeMirror.setValue(newValue + formattedSql + ';')
+    }
+
     $.showCreateTableAjax = function (tableName) {
         $.ajax({
             type: 'POST',
             url: pathname + "/query",
-            data: { tid: activeMerchantId, sql: 'show create table ' + tableName},
+            data: {tid: activeMerchantId, sql: 'show create table ' + tableName},
             success: function (content, textStatus, request) {
                 var createTableSql = content.Rows[0][2]
-                var formattedSql = sqlFormatter.format(createTableSql, { language: 'sql' })
-                $.sqlCodeMirror.setValue(formattedSql + ';\n')
+                $.appendSqlToSqlEditor(createTableSql);
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 alert(jqXHR.responseText + "\nStatus: " + textStatus + "\nError: " + errorThrown)
