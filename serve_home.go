eb6fa9917caab4aac996c4e259288ff3a1a682f8
bingoo <bingoo.huang@gmail.com>
2018-04-05 14:46:00 +0800

    refactor

--- serve_home.go
+++ serve_home.go
@@ -12,11 +12,6 @@ import (
 )
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	indexHtml := string(MustAsset("res/index.html"))
