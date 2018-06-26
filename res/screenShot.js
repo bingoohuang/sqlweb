a71db46bc651aa86a2266e299a371872bec5e1d8
bingoo <bingoo.huang@gmail.com>
2018-06-26 16:01:24 +0800

    lazy html2canvas

--- res/screenShot.js
+++ res/screenShot.js
@@ -3,13 +3,24 @@
         $('#screenShot').hide()
     })
 
-
-    $.screenShot = function (resultId) {
+    function executeScreenShot(resultId) {
         $('#collapseDiv' + resultId).removeClass('collapseDiv')
-        html2canvas($('#queryResult' + resultId)[0]).then(canvas => {
+        html2canvas($('#queryResult' + resultId)[0]).then(function (canvas) {
             $('#screenShot').show()
             $('#screenShotCanvas').html($(canvas))
         })
+    }
+
+    $.screenShot = function (resultId) {
+        if (typeof html2canvas == "undefined") {
+            $.getScript("https://cdn.bootcss.com/html2canvas/0.5.0-beta4/html2canvas.min.js")
+                .done(function (script, textStatus) {
+                    executeScreenShot(resultId)
+                })
+        } else {
+            executeScreenShot(resultId)
+        }
+
     }
 })()
 
