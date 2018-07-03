d8523d39457169cf014ff324cd471001652eedab
bingoo <bingoo.huang@gmail.com>
2018-07-03 19:42:08 +0800

    refactor deploy shell

--- deploy-gw01.to.smc01.sh
+++ deploy-gw01.to.smc01.sh
@@ -6,6 +6,6 @@ ssh -tt smc01 "bash -s" << eeooff
 cd app/$deployName
 ps -ef|grep $deployName|grep -v grep|awk '{print \$2}'|xargs -r kill -9
 cp -f ~/$deployName.linux.bin .
-./start-go-sql-web.sh
+./start-$deployName.sh
 exit
 eeooff
