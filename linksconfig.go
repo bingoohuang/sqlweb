3d365caebaa4e2a043aad92b5e1d1f16d4f83c50
bingoohuang <bingoo.huang@gmail.com>
2020-10-14 18:25:15 +0800

    create demo linksConfigFile

--- linksconfig.go
+++ linksconfig.go
@@ -13,6 +13,56 @@ import (
 
 const linksConfigFile = "linksConfig.toml"
 
+func init() {
+	if _, err := os.Stat(linksConfigFile); !os.IsNotExist(err) {
+		return
+	}
+	ioutil.WriteFile(linksConfigFile, []byte(`
+[tables] # 单表查询时附加语句配置
+	[tables.hogan_pdf]
+    replaceSql = "select user_id, pdf_name, '(BLOB)' pdf_content, CREATE_TIME from hogan_pdf"
+    [tables.f_file]
+    replaceSql = "select file_id, file_name, '(BLOB)' file_content from f_file"
+
+[links]
+    [links.user_id-yoga]
+    classifiers = ["yoga", "yoga-dev"]
+    linksTo = ["tt_f_user", "tt_f_user_role", "tt_f_member", "tt_f_mbr_card.member_id", "tt_f_coach", "tt_f_staff", "tt_f_subscribe", "tt_f_login"]
+    
+    [links.MBR_CARD_ID]
+    classifiers = ["yoga", "yoga-dev"]
+    linksTo = ["tt_f_mbr_card", "tt_l_mbrcard_chg"]
+
+[entries]
+    [entries.findTenant]
+    classifiers = ["trr"]
+    type = "input"
+    autoHide  = true
+    placeholder = "逗号分隔的classifier"
+    label = "找库"
+    userTemplate = true
+    sql = "select * from tr_f_merchant where classifier in ({{input|splitToInClause}})"
+
+    [entries.member]
+    type = "input"
+    placeholder = "手机号码/姓名"
+    label = "查找用户"
+    sql = "select * from tt_f_user where nickname like '%{input}%' or mobile like '%{input}%'"
+
+    [entries.merchantYoga]
+    classifiers = ["yoga", "yoga-dev"]
+    type = "link"
+    label = "找馆主"
+    sql = "select * from tt_f_user_role where role_id = '100'"
+    
+    [entries.merchantOther]
+    excludeClassifiers = ["yoga", "yoga-dev"]
+    type = "link"
+    label = "找馆主"
+    sql = "select * from easyhi_user where user_id in (select user_id from easyhi_user_role where role_name = '馆主')"
+`), 0644)
+}
+
 func ServeSaveLinksConfig(w http.ResponseWriter, r *http.Request) {
 	linksConfig := r.FormValue("linksConfig")
 
